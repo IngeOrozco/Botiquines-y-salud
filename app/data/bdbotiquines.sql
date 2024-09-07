7f48a8b8da6e1ff21ddd8b355f1519e40398c514
IngeOrozco <lect.orozco@gmail.com>
2024-09-07 09:59:00 -0500

    #

--- app/data/bdbotiquines.sql
+++ app/data/bdbotiquines.sql
@@ -18,47 +18,47 @@ s => sEjm_Ejm // Documento
 /* ######################### Tablas #########################*/
 CREATE TABLE estado (
     pk_id_etd INT AUTO_INCREMENT PRIMARY KEY,
-
+    #
     nom_etd VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE administrador (
     pk_id_adm INT AUTO_INCREMENT PRIMARY KEY,
-
+    #
     nom_adm VARCHAR(150) NOT NULL,
     ape_adm VARCHAR(150) NOT NULL,
     usu_adm VARCHAR(50) NOT NULL,
     ema_adm VARCHAR(150) NOT NULL,
     pass_adm VARCHAR(200) NOT NULL,
-
+    #
     fk_etd_adm INT DEFAULT 1 NOT NULL,
-
+    #
     CONSTRAINT admEtd FOREIGN KEY(fk_etd_adm) REFERENCES estado(pk_id_etd)
 );
 
 CREATE TABLE categoria (
     pk_id_ctg INT AUTO_INCREMENT PRIMARY KEY,
-
+    #
     nom_ctg VARCHAR(250) NOT NULL,
     img_ctg VARCHAR(150),
     des_ctg TEXT,
-
+    #
     fk_etd_ctg INT DEFAULT 1 NOT NULL,
-
+    #
     CONSTRAINT ctgEtd FOREIGN KEY(fk_etd_ctg) REFERENCES estado(pk_id_etd)
 );
 
 CREATE TABLE producto (
     pk_id_prd INT AUTO_INCREMENT PRIMARY KEY,
-
+    #
     dCrt_prd DATE NOT NULL,
     nom_prd VARCHAR(200) NOT NULL,
     img_prd TEXT,
     des_prd TEXT,
-
+    #
     fk_ctg_prd INT DEFAULT 1 NOT NULL,
     fk_etd_prd INT DEFAULT 1 NOT NULL,
-
+    #
     CONSTRAINT prdCtg FOREIGN KEY(fk_ctg_prd) REFERENCES categoria(pk_id_ctg),
     CONSTRAINT prdEtd FOREIGN KEY(fk_etd_prd) REFERENCES estado(pk_id_etd)
 );
