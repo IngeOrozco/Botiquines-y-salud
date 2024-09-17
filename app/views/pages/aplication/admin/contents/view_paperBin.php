4272fd0c9363cd0b52440d94c03535b549122613
IngeOrozco <lect.orozco@gmail.com>
2024-09-16 22:22:04 -0500

    responsive fixed Beta 0.1

--- app/views/pages/aplication/admin/contents/view_paperBin.php
+++ app/views/pages/aplication/admin/contents/view_paperBin.php
@@ -1,45 +1,49 @@
 <section class="space"></section>
 <main id="paperBin">
-    <div class="form-bin">
-        <?php
-        if (!empty($class)) {
-            if ($class == 'Admin') {
-                $ctrll = 'administrador';
-            } else if ($class == 'Category') {
-                $ctrll = 'categoria';
-            } else if ($class == 'Product') {
-                $ctrll = 'producto';
-            }
-
-            if (!empty($ctrll)) {
-        ?>
-                <form class="ajax-form paperBin" action="?ajax=<?= $ctrll ?>&&func=paperBin" data-redirect=".alert">
-                    <div class="alert"></div>
-                    <?php
-                    require_once('app/views/pages/aplication/admin/components/forms/cont_bin' . $class . '.php');
-                    ?>
-                </form>
-        <?php
+    <div class="content-bar">
+        <div class="form-bin content-table">
+            <?php
+            if (!empty($class)) {
+                if ($class == 'Admin') {
+                    $ctrll = 'administrador';
+                } else if ($class == 'Category') {
+                    $ctrll = 'categoria';
+                } else if ($class == 'Product') {
+                    $ctrll = 'producto';
+                }
+    
+                if (!empty($ctrll)) {
+            ?>
+                    <form class="ajax-form paperBin" action="?ajax=<?= $ctrll ?>&&func=paperBin" data-redirect=".alert">
+                        <div class="alert"></div>
+                        <?php
+                        require_once('app/views/pages/aplication/admin/components/forms/cont_bin' . $class . '.php');
+                        ?>
+                    </form>
+            <?php
+                } else {
+                    echo '<h5 class="notFound tt-5">Contacta a los desarrolladores</h5>';
+                }
             } else {
-                echo '<h5 class="notFound tt-5">Contacta a los desarrolladores</h5>';
+                echo '<h5 class="notFound tt-5">Ninguna visualización</h5>';
             }
-        } else {
-            echo '<h5 class="notFound tt-5">Ninguna visualización</h5>';
-        }
-        ?>
-    </div>
-    <div class="slide-menu">
-        <ul>
-            <li>
-                <a href="?ctrll=categoria&&func=paperBin">Categorias</a>
-            </li>
-            <li>
-                <a href="?ctrll=producto&&func=paperBin">Productos</a>
-            </li>
-            <li>
-                <a href="?ctrll=administrador&&func=paperBin">Administradores</a>
-            </li>
-        </ul>
+            ?>
+            <br>
+            <p class="tx">Si no se alcanzan a ver los datos, por favor mover hacia la izquierda para ver los demás.</p>
+        </div>
+        <div class="slide-menu">
+            <ul>
+                <li>
+                    <a href="?ctrll=categoria&&func=paperBin">Categorias</a>
+                </li>
+                <li>
+                    <a href="?ctrll=producto&&func=paperBin">Productos</a>
+                </li>
+                <li>
+                    <a href="?ctrll=administrador&&func=paperBin">Administradores</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </main>
 <section class="space"></section>
--- app/views/pages/general/admin/contents/view_admin.php
+++ app/views/pages/general/admin/contents/view_admin.php
@@ -1,46 +1,50 @@
 <section class="space"></section>
-<main id="list-admin" class="content-table">
-    <table>
-        <thead>
-            <tr>
-                <th class="max">#</th>
-                <th>@Usuario</th>
-                <th>Nombre/s</th>
-                <th>Apellido/s</th>
-                <th>Correo</th>
-                <th>Estado</th>
-                <th><a class="btn blue" href="?ctrll=administrador&&func=crud">Agregar</a></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            if ($countAdm > 0) {
-                $fila = 1;
-
-                foreach ($listAdm as $rowAdm) {
-            ?>
-                    <tr>
-                        <td class="center"><?= ($fila++) ?></td>
-                        <td><?= $rowAdm['usu_adm'] ?></td>
-                        <td><?= $rowAdm['nom_adm'] ?></td>
-                        <td><?= $rowAdm['ape_adm'] ?></td>
-                        <td><?= $rowAdm['ema_adm'] ?></td>
-                        <td><?= $rowAdm['fk_etd_adm'] == 1 ? 'Activo' : 'Inactivo' ?></td>
-                        <td><a class="btn green" href="?ctrll=administrador&&func=crud&&adm=<?= $rowAdm['pk_id_adm'] ?>">Modificar</a></td>
-                    </tr>
-            <?php
+<main id="list-admin">
+    <div class="content-table">
+        <table>
+            <thead>
+                <tr>
+                    <th class="max">#</th>
+                    <th>@Usuario</th>
+                    <th>Nombre/s</th>
+                    <th>Apellido/s</th>
+                    <th>Correo</th>
+                    <th>Estado</th>
+                    <th><a class="btn blue" href="?ctrll=administrador&&func=crud">Agregar</a></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($countAdm > 0) {
+                    $fila = 1;
+    
+                    foreach ($listAdm as $rowAdm) {
+                ?>
+                        <tr>
+                            <td class="center"><?= ($fila++) ?></td>
+                            <td><?= $rowAdm['usu_adm'] ?></td>
+                            <td><?= $rowAdm['nom_adm'] ?></td>
+                            <td><?= $rowAdm['ape_adm'] ?></td>
+                            <td><?= $rowAdm['ema_adm'] ?></td>
+                            <td><?= $rowAdm['fk_etd_adm'] == 1 ? 'Activo' : 'Inactivo' ?></td>
+                            <td><a class="btn green" href="?ctrll=administrador&&func=crud&&adm=<?= $rowAdm['pk_id_adm'] ?>">Modificar</a></td>
+                        </tr>
+                <?php
+                    }
+                } else {
+                    echo '<tr><td class="notFound" colspan="7"><h5 class="tt-4">Ningun usuario registrado hasta el momento</h5></td></tr>';
                 }
-            } else {
-                echo '<tr><td class="notFound" colspan="7"><h5 class="tt-4">Ningun usuario registrado hasta el momento</h5></td></tr>';
-            }
-            ?>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="6"></td>
-                <td><a class="btn gray" href="?ctrll=administrador&&func=paperBin">Papelera</a></td>
-            </tr>
-        </tfoot>
-    </table>
+                ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="6"></td>
+                    <td><a class="btn gray" href="?ctrll=administrador&&func=paperBin">Papelera</a></td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+    <br>
+    <p class="tx">Si no se alcanzan a ver los datos, por favor mover hacia la izquierda para ver los demás.</p>
 </main>
 <section class="space"></section>
